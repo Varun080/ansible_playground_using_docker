d2f83b75dffbefcd90bf17c63ce4b2b55930187f
Varun <varunpandey080@gmail.com>
2024-08-03 16:36:25 +0530

    added playbooks and updated docker_exec.sh to run and configur ssh for host nodes

--- docker_exec.sh
+++ docker_exec.sh
@@ -2,6 +2,7 @@
 host_ps=$(docker ps -q -f ancestor=ansible_host_node:compose_0)
 for ps in $host_ps;do
     docker exec $ps service ssh start
+    docker exec $ps apt update && docker exec $ps apt install -y curl gpg
 done
 control_ps=$(docker ps -q -f ancestor=ansible_control_node:compose_0)
 docker exec $control_ps python3 /home/ansuser/ssh_push/sshkey_push.py
